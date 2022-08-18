59d50e03f9571f4fdd9da68e1a1f8db564b54145
GunHo <noh3310@gmail.com>
2022-08-18 18:45:52 +0900

    first commit

---
+++ CombineProject/CombineProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineProjectApp.swift
+//  CombineProject
+//
+//  Created by 노건호 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct CombineProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
