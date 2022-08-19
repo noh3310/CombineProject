062697d2c6e7bcbfef54fbc5f5c3d094d8b3f85e
GunHo <noh3310@gmail.com>
2022-08-19 18:07:47 +0900

    #1 feat: SearchBar 구현

---
+++ CombineProject/View/Home/Home.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  CombineProject
+//
+//  Created by 노건호 on 2022/08/18.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    @State private var searchText = ""
+    
+    var body: some View {
+        if #available(iOS 15.0, *) {
+            NavigationView {
+                HomeView()
+                    .navigationTitle("Search")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $searchText)
+        } else {
+            NavigationView {
+                HomeView()
+                    .navigationTitle("Search")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+        }
+    }
+}
+
+struct HomeView: View {
+    
+    @State private var searchText = ""
+    
+    var body: some View {
+        VStack {
+            if #unavailable(iOS 15.0) {
+                SearchBar(text: $searchText)
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+            }
+            Spacer()
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
