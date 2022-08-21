9febfaa05d3da9c605e31fd1229a8953bce3a0b1
GunHo <noh3310@gmail.com>
2022-08-21 22:59:16 +0900

    #2 feat: SearchBar 값 변경되었을 때 호출기능 구현

--- CombineProject/View/Home/Home.swift
+++ CombineProject/View/Home/Home.swift
@@ -9,25 +9,19 @@ import SwiftUI
 
 struct Home: View {
     
-    @State var searchText = ""
-    let apiManager = APIManager()
+    @StateObject var viewModel = ViewModel()
     
     var body: some View {
         if #available(iOS 15.0, *) {
             NavigationView {
-                HomeView()
+                HomeView(viewModel: viewModel)
                     .navigationTitle("Search")
                     .navigationBarTitleDisplayMode(.inline)
-                    .toolbar {
-                        Button("검색") {
-                            apiManager.searchRepositorys(p: "hihi")
-                        }
-                    }
             }
-            .searchable(text: $searchText)
+            .searchable(text: $viewModel.searchText)
         } else {
             NavigationView {
-                HomeView()
+                HomeView(viewModel: viewModel)
                     .navigationTitle("Search")
                     .navigationBarTitleDisplayMode(.inline)
             }
@@ -37,15 +31,17 @@ struct Home: View {
 
 struct HomeView: View {
     
-    @State private var searchText = ""
+    @ObservedObject var viewModel: ViewModel
     
     var body: some View {
         VStack {
             if #unavailable(iOS 15.0) {
-                SearchBar(text: $searchText)
+                SearchBar(text: $viewModel.searchText)
                     .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
             }
-            Spacer()
+            List(viewModel.repos, id: \.url) { repo in
+                TableViewCell(title: repo.fullName, bodyText: repo.url)
+            }.listStyle(.plain)
         }
     }
 }
